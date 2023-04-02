1bf15ee193f88e455b991fa6a9e750db322db231
Seth McCullough <seth@endevrs.dev>
2023-04-02 15:24:32 -0400

    Install drei and remove excess components

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,47 +1,16 @@
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-import CustomObject from "@/components/CustomObject";
-
-extend({ OrbitControls });
-
 export default function Experience() {
-  const { camera, gl } = useThree();
-
-  const cubeRef = useRef<THREE.Mesh>(null);
-  const groupRef = useRef<THREE.Group>(null);
-
-  useFrame((state, delta) => {
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-
-    if (cubeRef.current) {
-      cubeRef.current.rotation.y += delta;
-    }
-  });
-
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
-
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <group ref={groupRef}>
+      <group>
         <mesh position-x={-2}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh
-          ref={cubeRef}
-          rotation-y={Math.PI * 0.25}
-          position-x={2}
-          scale={1.5}
-        >
+        <mesh position-x={2} scale={1.5}>
           <boxGeometry />
           <meshStandardMaterial color="mediumpurple" />
         </mesh>
@@ -51,8 +20,6 @@ export default function Experience() {
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
       </mesh>
-
-      <CustomObject />
     </>
   );
 }
